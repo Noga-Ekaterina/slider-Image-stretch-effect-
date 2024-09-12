d88459e4bcbb75a43025a356ba5654777f3a5eda
Noga-Ekaterina <katrin210607@gmail.com>
2024-09-13 02:49:18 +0300

    убрала бесконечность

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const slides = document.querySelectorAll(".slide");
-
+const slides = document.querySelectorAll(".slider__item");
+const slider=document.querySelector(".slider__vrapp")
 
 let current = 0;
 let prev;
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded',()=>{
    gotoNum(current);
 });
 
-const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoPrev = () => current > 0 && gotoNum(current - 1);
 
-const gotoNext = () => current < slides.length - 1 ? gotoNum(current + 1) : gotoNum(0);
+const gotoNext = () => current < slides.length - 1 && gotoNum(current + 1);
 
 const gotoNum = number => {
    current = number;
@@ -20,44 +20,53 @@ const gotoNum = number => {
    next = current + 1;
    prev2 = current - 2;
    next2 = current + 2;
+   const resizeSlides = (dir) => {
+      const averageSlide= slides[dir=="botton"? current+1:current-1]
+      const size= slides[slides.length-1].clientHeight
 
-   for (let i = 0; i < slides.length; i++) {
+      if (dir=="botton"){
+         averageSlide.nextElementSibling.style.height=`${size-size/3}px`
+      }else
+         averageSlide.previousElementSibling.style.height=`${size-size/3}px`
+
+      averageSlide.style.height=`${size+size/3}px`
+      averageSlide.style.maxHeight=`${size+size/3}px`
+
+   }
+
+   for (let i =
+       0; i < slides.length; i++) {
       slides[i].classList.remove("active");
       slides[i].classList.remove("prev");
       slides[i].classList.remove("next");
       slides[i].classList.remove("prev-2");
       slides[i].classList.remove("next-2");
+      slides[i].style.height=""
+      slides[i].style.maxHeight=""
    }
 
-   if (next == slides.length) {
-      next = 0;
-   }
-
-   if (next2 >= slides.length -1) {
-      next2 = next<slides.length-1? next+1:0
-   }
-
-   if (prev == -1) {
-      prev = slides.length - 1;
-   }
-
-   if (prev2 <= -1) {
-      prev2 = prev!=0? prev-1 :slides.length-1;
-   }
-
-   console.log("p-" + prev + ", n-" + next + ", p2-" + prev2 + ", n2-" + next2);
    window.removeEventListener("wheel", handleWheel)
    setTimeout(()=>{
       window.addEventListener("wheel", handleWheel)
    }, 500)
 
+   if (current>0 && current<slides.length-1){
+      console.log("step")
+      slider.style.transform=`translateY(-${slides[0].clientHeight *(current-1)}px)`
+   }else if (current==0){
+      resizeSlides("botton")
+   }else if (current==slides.length-1){
+      resizeSlides("top")
+   }
+
    slides[current].classList.add("active");
-   slides[prev].classList.add("prev");
-   slides[next].classList.add("next");
-   slides[prev2].classList.add("prev-2");
-   slides[next2].classList.add("next-2");
+   // slides[prev].classList.add("prev");
+   // slides[next].classList.add("next");
+   // slides[prev2].classList.add("prev-2");
+   // slides[next2].classList.add("next-2");
 
 }
+
 
 const handleWheel = (ev) => {
 
